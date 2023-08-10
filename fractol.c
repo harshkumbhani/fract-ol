a44280df3bf05bbde143c112ce12d004957c011b
Harsh Kumbhani <harshwut@icloud.com>
2023-08-10 09:21:16 +0200

    Added MLX42 as submodule

--- fractol.c
+++ fractol.c
@@ -6,20 +6,41 @@
 /*   By: hkumbhan <hkumbhan@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 13:11:31 by hkumbhan          #+#    #+#             */
-/*   Updated: 2023/08/09 14:14:26 by hkumbhan         ###   ########.fr       */
+/*   Updated: 2023/08/10 09:04:36 by hkumbhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+
+typedef struct s_color
+{
+	char a;
+	char b;
+	char g;
+	char r;
+}	t_color;
+
+union u_color
+{
+	int i;
+	t_color c;
+};
+
 int	main(int argc, char *argv[])
 {
 	(void)argc;
 	(void)argv;
-	mlx_t	*mlx;
+	mlx_t		*mlx;
 	mlx_image_t *img;
 	int			x;
 	int			y;
+
+	union u_color color;
+	color.i = 0;
+	color.c.a = 0xff;
+	color.c.r = 0xff;
+	
 
 	x = 0;
 	y = 0;
@@ -33,12 +54,15 @@ int	main(int argc, char *argv[])
 		y = 0;
 		while (y < HEIGHT)
 		{
+			color.c.b = (char)((double)(x) * .25);
+			color.c.g = (char)((double)(y) * .25);
 			// Apply the fractol function
-			mlx_put_pixel(img, x, y, 0xFF0000FF);
+			mlx_put_pixel(img, x, y, color.i);
 			y++;
 		}
 		x++;
 	}
 	mlx_loop(mlx);
+	mlx_terminate(mlx);
 	return (0);
 }
