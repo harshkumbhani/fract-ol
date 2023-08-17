e628f20abf76e5531067222eed9a9f56b8fd37c6
Harsh Kumbhani <Harshwut@icloud.com>
2023-08-17 09:35:36 +0200

    git removed cached

--- fractol.c
+++ fractol.c
@@ -6,7 +6,17 @@
 /*   By: hkumbhan <hkumbhan@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 09:23:46 by hkumbhan          #+#    #+#             */
-/*   Updated: 2023/08/17 09:23:47 by hkumbhan         ###   ########.fr       */
+/*   Updated: 2023/08/17 09:27:38 by hkumbhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fractol.h"
+
+int	main(int argc, char *argv[])
+{
+	t_fractal	fractal;
+
+	if (argc < 2)
+		// open manual and print manual
+	ft_init(&fractal, argv);
+}
